676148db46a07a890218e7ec3526dd5adde4c5ca
Gaujap <debarnot.gabriel@gmail.com>
2024-10-27 15:06:41 +0100

    Edit "moveUp" logic

--- Game.java
+++ Game.java
@@ -26,6 +26,8 @@ public class Game {
     }
 
     private void moveUp() {
+        boolean moved = false;
+
         for (int i = 0; i < size; i++) {
             for (int j = 1; j < size; j++) {
                 if (board[i][j] != 0) {
@@ -34,18 +36,43 @@ public class Game {
                         board[k - 1][j] = board[k][j];
                         board[k][j] = 0;
                         k--;
-                    }
-                    if (k > 0 && board[k-1][j] == board[k][j]) {
-                        board[k - 1][j] *= 2;
-                        board[k][j] = 0;
+                        moved = true;
                     }
                 }
             }
         }
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (board[i][j] != 0 && i > 0 && board[i - 1][j] == board[i][j]) {
+                    board[i - 1][j] *= 2;
+                    board[i][j] = 0;
+                    moved = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 1; j < size; j++) {
+                if (board[i][j] != 0) {
+                    int k = i;
+                    while (k > 0 && board[k - 1][j] == 0) {
+                        board[k - 1][j] = board[k][j];
+                        board[k][j] = 0;
+                        k--;
+                        moved = true;
+                    }
+                }
+            }
+        }
+
+        if (moved) {
+            addRandomTile();
+        }
     }
 
     private void moveDown() {
-        for (int i = 0; i < size ; i++) {
+        for (int i = 0; i < size; i++) {
             for (int j = 0; j < size ; j++) {
                 if (board[i][j] != 0) {
                     int k = i;
