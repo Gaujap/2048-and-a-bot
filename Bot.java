3ce0cf46b8c70378cca05fae7a1911be9bca90f4
Gaujap <debarnot.gabriel@gmail.com>
2024-10-27 20:26:48 +0100

    Add "getBestMove" method, only random movements

--- Bot.java
+++ Bot.java
@@ -1,7 +1,14 @@
+import java.util.Random;
+
 public class Bot {
     private Game game;
 
     public Bot(Game game) {
         this.game = game;
+    }
+
+    public String getBestMove() {
+        String[] directions = {"z", "s", "q", "d"};
+        return directions[new Random().nextInt(directions.length)];
     }
 }
