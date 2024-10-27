dcb62389e990413577b2e6b22814ea964491581b
Gaujap <debarnot.gabriel@gmail.com>
2024-10-27 14:47:43 +0100

    Add "printBoard" logic

--- Game.java
+++ Game.java
@@ -51,7 +51,12 @@ public class Game {
     }
 
     public void printBoard() {
-        // Print the board
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
     public void printScore() {
